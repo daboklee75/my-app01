630b7d99fec66cd9451071b2c504a67689ee64b8
daboklee75 <daboklee75@hanmail.net>
2025-09-30 02:33:10 +0900

    test

--- src/pages/app/body.tsx
+++ src/pages/app/body.tsx
@@ -1,13 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
 
 function Body() {
 
+    const [isLoading, setIsLoading] = useState(true);
+    const [count, setCount] = useState(0);
+
+//    setIsLoading(false);
     useEffect(() => {
-        console.log("렌더링 때마다 실행");
-    });
+        console.log("1");
+
+        (async() => {
+            console.log("2");
+        })();
+
+        },  []
+    ); // 최초 1회 실행
+
+    console.log("isLoading", isLoading);
+    // 로딩
+    // if (isLoading) {
+    //     return (
+    //         <div>loading</div>
+    //     );
+    // }
 
     return (
-        <div>body</div>
+        <div>
+            <p>현재 값: {count}</p>
+            <button onClick={() => setCount(count)}>증가</button>
+        </div>
+    );
+
+
+    return (
+      <div>xxx</div>
     );
 }
 
